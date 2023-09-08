21934e36c739343327af42179642b176cc1e4b7d
Pecherky Oleg <yogidanfuzo@gmail.com>
2023-09-08 18:57:35 +0300

    Small changes in [MainWindow.xaml.cs]
    1. Remove old hardcode section with courses and modify a new list<Course> section
    2. Also added a not for future testing

--- DesktopApplication/MainWindow.xaml.cs
+++ DesktopApplication/MainWindow.xaml.cs
@@ -19,104 +19,6 @@ public partial class MainWindow : Window
 
         DataContext = _dataRepository.Courses;
 
-        /*var courses = new List<Course>
-        {
-            new()
-            {
-                CourseId = 1,
-                CourseName = "System Engineer",
-                Groups = new List<Group>
-                {
-                    new ()
-                    {
-                        GroupId = 11,
-                        GroupName = "SSE-22",
-                        Students = new List<Student>
-                        {
-                            new() {StudentId = 0, StudentFullName = "FillFullName", isWorkingInDepartment = false},
-                            new() {StudentId = 0, StudentFullName = "FillFullName", isWorkingInDepartment = false},
-                            new() {StudentId = 0, StudentFullName = "FillFullName", isWorkingInDepartment = false},
-                            new() {StudentId = 0, StudentFullName = "FillFullName", isWorkingInDepartment = false}
-                        }
-                    },
-                    new ()
-                    {
-                        GroupId = 12,
-                        GroupName = "SSE-11",
-                        Students = new List<Student>
-                        {
-                            new() {StudentId = 111, StudentFullName = "FillFullName", isWorkingInDepartment = false},
-                            new() {StudentId = 111, StudentFullName = "FillFullName", isWorkingInDepartment = false},
-                            new() {StudentId = 111, StudentFullName = "FillFullName", isWorkingInDepartment = false},
-                            new() {StudentId = 111, StudentFullName = "FillFullName", isWorkingInDepartment = false}
-                        }
-                    },
-                    new ()
-                    {
-                        GroupId = 13,
-                        GroupName = "SSE-12",
-                        Students = new List<Student>
-                        {
-                            new() {StudentId = 111, StudentFullName = "FillFullName", isWorkingInDepartment = false},
-                            new() {StudentId = 111, StudentFullName = "FillFullName", isWorkingInDepartment = true},
-                            new() {StudentId = 111, StudentFullName = "FillFullName", isWorkingInDepartment = true},
-                            new() {StudentId = 111, StudentFullName = "FillFullName", isWorkingInDepartment = false}
-                        }
-                    },
-                }
-            },
-            new()
-            {
-                CourseId = 2,
-                CourseName = "Software Engineer",
-                Groups = new List<Group>
-                {
-                    new() {GroupId = 21, GroupName = "SWE-11"},
-                    new() {GroupId = 22, GroupName = "SWE-12"},
-                    new() {GroupId = 23, GroupName = "SWE-13"},
-                    new() {GroupId = 24, GroupName = "SWE-31"},
-                    new() {GroupId = 25, GroupName = "SWE-32"},
-                    new() {GroupId = 26, GroupName = "SWE-21"},
-                    new() {GroupId = 27, GroupName = "SWE-22"}
-                }
-            },
-            
-            new()
-            {
-                CourseId = 3,
-                CourseName = "Data Science",
-                Groups = new List<Group>
-                {
-                    new() {GroupId = 31, GroupName = "DS-41"},
-                    new() {GroupId = 32, GroupName = "DS-31"},
-                    new() {GroupId = 33, GroupName = "DS-11"},
-                    new() {GroupId = 34, GroupName = "DS-12"}
-                }
-            },
-            
-            new()
-            {
-                CourseId = 4,
-                CourseName = "Data Analysis",
-                Groups = new List<Group>
-                {
-                    new() {GroupId = 41, GroupName = "DA-31"},
-                    new() {GroupId = 42, GroupName = "DA-32"},
-                    new() {GroupId = 43, GroupName = "DA-21"}
-                }
-            },
-            
-            new()
-            {
-                CourseId = 5,
-                CourseName = "Cyber Security",
-                Groups = new List<Group>
-                {
-                    new() {GroupId = 51, GroupName = "CS-21"}
-                }
-            },
-        };*/
-
         CourseListView.SelectionChanged += CourseListView_SelectionChanged;
         
         GroupListView.SelectionChanged += GroupListView_SelectionChanged;
@@ -124,30 +26,37 @@ public partial class MainWindow : Window
 
     private void InitializeData()
     {
-        Course course = new Course
+        var courses = new List<Course>
         {
-            CourseId = 1,
-            CourseName = "System Engineer"
+            new() { CourseId = 1, CourseName = "System Engineer" },
+            new() { CourseId = 2, CourseName = "Software Engineer" },
+            new() { CourseId = 3, CourseName = "Data Science" },
+            new() { CourseId = 4, CourseName = "Data Analysis"},
+            new() { CourseId = 5, CourseName = "Cyber Security"}
         };
 
+        // VVV Hardcoded group and student below only for testing purpose VVV
+        
         Group group = new Group
         {
             GroupId = 11,
-            GroupName = "SSE-22"
+            GroupName = "Test Group"
         };
 
         Student student1 = new Student
         {
             StudentId = 1,
-            StudentFullName = "Student 1",
+            StudentFullName = "Test Student 1",
             isWorkingInDepartment = false
         };
         
         group.Students.Add(student1);
         
-        course.Groups.Add(group);
-        
-        _dataRepository.Courses.Add(course);
+        foreach (var course in courses)
+        {
+            course.Groups.Add(group);
+            _dataRepository.Courses.Add(course);
+        }
     }
 
     private void CourseListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
