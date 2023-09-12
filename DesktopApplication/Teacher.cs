e2b536ca4d908e472521709f48fa97f326c1b678
Pecherky Oleg <yogidanfuzo@gmail.com>
2023-09-12 18:17:10 +0300

    Teacher class is created to working with it further

---
+++ DesktopApplication/Teacher.cs
@@ -0,0 +1,8 @@
+namespace DesktopApplication;
+
+public class Teacher
+{ 
+    public int TeacherId { get; set; } 
+    public string TeacherFullName { get; set; }
+    public bool IsCorrespondence { get; set; }
+}
