a16c5476287f6ea204057886fa4a91c5f0677e19
Pecherky Oleg <yogidanfuzo@gmail.com>
2023-09-12 16:13:38 +0300

    Change Course.cs to held list of Teachers

--- DesktopApplication/Course.cs
+++ DesktopApplication/Course.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Windows.Documents;
 
 namespace DesktopApplication;
     
@@ -7,11 +8,13 @@ public class Course
     public int CourseId { get; set; }
     public string CourseName { get; set; }
     public List<Group> Groups { get; set; }
+    public List<Teacher> Teachers { get; set; }
     public int LastUsedGroupId { get; set; }
 
     public Course()
     {
         Groups = new List<Group>();
+        Teachers = new List<Teacher>();
         LastUsedGroupId = 0;
     }
 }
