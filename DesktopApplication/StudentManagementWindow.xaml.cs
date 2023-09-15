1fd91474260e1f5c73f85c6e3c2d58e03b740587
Pecherky Oleg <yogidanfuzo@gmail.com>
2023-09-15 18:45:28 +0300

    Modified overal view of [ManageStudentGroupPage.xaml] and adding a page support

--- DesktopApplication/StudentManagementWindow.xaml.cs
+++ DesktopApplication/StudentManagementWindow.xaml.cs
@@ -1,5 +1,4 @@
 using System.Windows;
-using System.Linq;
 using System.Collections.Generic;
 
 namespace DesktopApplication;
@@ -11,88 +10,25 @@ public partial class StudentManagementWindow : Window
 
     public StudentManagementWindow(DataRepository dataRepository, HashSet<Student> assignedStudents)
     {
-        InitializeComponent();
         _dataRepository = dataRepository;
-        this._assignedStudents = assignedStudents;
-        GroupComboBox.ItemsSource = _dataRepository.Groups;
-        StudentsListView.ItemsSource = _dataRepository.Students;
+        _assignedStudents = assignedStudents;
+        InitializeComponent();
     }
 
-    private void AddStudentsToGroup_Click(object sender, RoutedEventArgs e)
+    private void ManageStudentsGroupButton_Click(object sender, RoutedEventArgs e)
     {
-        var selectedGroup = GroupComboBox.SelectedItem as Group;
-
-        if (selectedGroup != null)
-        {
-            var selectedStudents = StudentsListView.SelectedItems.OfType<Student>().ToList();
-
-            if (selectedStudents.Count > 0)
-            {
-                foreach (var student in selectedStudents)
-                {
-                    if (student.CurrentGroupName == null && !_assignedStudents.Contains(student))
-                    {
-                        student.CurrentGroupName = selectedGroup.GroupName;
-                        selectedGroup.Students.Add(student);
-
-                        _assignedStudents.Add(student);
-                    }
-                    else
-                    {
-                        MessageBox.Show("Student '" + student.StudentFullName + "' is already assigned to a group '" + student.CurrentGroupName + "'", "Error",
-                            MessageBoxButton.OK, MessageBoxImage.Error);
-                    }
-                }
-            
-                StudentsListView.Items.Refresh();
-
-                StudentsListView.SelectedItems.Clear();
-            }
-            else
-            {
-                MessageBox.Show("Please, select a student from the list to add to the group", "Error",
-                    MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-        else
-        {
-            MessageBox.Show("Please, select a group to add students to", "Error",
-                MessageBoxButton.OK, MessageBoxImage.Error);
-        }
+        var manageStudentsGroupPage = new ManageStudentsGroupPage(_dataRepository, _assignedStudents);
+        MainFrame.Content = manageStudentsGroupPage;
     }
 
-    private void DeleteStudentsFromGroup_Click(object sender, RoutedEventArgs e)
+    private void EditStudentsButton_Click(object sender, RoutedEventArgs e)
     {
-        var selectedStudents = StudentsListView.SelectedItems.OfType<Student>().ToList();
+        var manageStudentsGroupPage = new EditStudentPage(_dataRepository);
+        MainFrame.Content = manageStudentsGroupPage;
+    }
 
-        if (selectedStudents.Count > 0)
-        {
-            foreach (var student in selectedStudents)
-            {
-                Group selectedGroup = _dataRepository.Groups.FirstOrDefault(group => group.Students.Contains(student));
+    private void ChangeStudentData_Click(object sender, RoutedEventArgs e)
+    {
 
-                if (selectedGroup != null)
-                {
-                    selectedGroup.Students.Remove(student);
-                    student.CurrentGroupName = null;
-
-                    _assignedStudents.Remove(student);
-                }
-                else
-                {
-                    MessageBox.Show("This student is not assigned to any group", "Nothing to remove",
-                        MessageBoxButton.OK, MessageBoxImage.Exclamation);
-                }
-            }
-
-            StudentsListView.Items.Refresh();
-
-            StudentsListView.SelectedItems.Clear();
-        }
-        else
-        {
-            MessageBox.Show("Please, select a student from the list to remove it from the group", "Error",
-                MessageBoxButton.OK, MessageBoxImage.Error);
-        }
     }
 }
