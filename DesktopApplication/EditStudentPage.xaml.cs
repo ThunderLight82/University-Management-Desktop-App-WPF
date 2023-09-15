f6b796e247fc108aba13029767730cdf16be4726
Pecherky Oleg <yogidanfuzo@gmail.com>
2023-09-15 18:40:57 +0300

    [WiP] Create EditStudentPage.xaml to held future logic for student adding/deletion

---
+++ DesktopApplication/EditStudentPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DesktopApplication;
+
+public partial class EditStudentPage : Page
+{
+    private DataRepository _dataRepository;
+
+    public EditStudentPage(DataRepository dataRepository) 
+    { 
+        InitializeComponent();
+        _dataRepository = dataRepository;
+    }
+
+    private void AddStudent_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void DeleteStudent_Click(object sender, RoutedEvent e)
+    {
+
+    }
+}
