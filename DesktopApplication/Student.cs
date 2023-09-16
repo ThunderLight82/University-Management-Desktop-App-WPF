e1b639366d488219658fcea27c3271ec778b0a90
Pecherky Oleg <yogidanfuzo@gmail.com>
2023-09-16 16:19:05 +0300

    Small change for null handling in  [Students.cs]

--- DesktopApplication/Student.cs
+++ DesktopApplication/Student.cs
@@ -4,6 +4,6 @@ public class Student
 { 
     public int StudentId { get; set; } 
     public string StudentFullName { get; set; }
-    public bool isWorkingInDepartment { get; set; }
+    public bool? isWorkingInDepartment { get; set; }
     public string CurrentGroupName { get; set; }
 }
