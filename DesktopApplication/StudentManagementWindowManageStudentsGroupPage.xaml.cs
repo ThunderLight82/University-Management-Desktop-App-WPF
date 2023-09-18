c0fcad945624d00bbe5da57bd79ab934e7c7baec
Pecherky Oleg <yogidanfuzo@gmail.com>
2023-09-18 18:07:47 +0300

    Refract the [StudentManagementWindowManageStudentGroup.xaml] and [StudentManagementWindowManageStudentGroup.xaml.cs].
    1. Added a new entry as to this class as "Course selection" to provide more targeting group selection that before
    2. Updated UI in .xaml to work with new implemented logic
    3. Fixed some naming typo
    4. Add aditional info text for user

---
+++ DesktopApplication/StudentManagementWindowManageStudentsGroupPage.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DesktopApplication;
+
+public partial class StudentManagementWindowManageStudentsGroupPage : Page
+{
+    private DataRepository _dataRepository;
+    private HashSet<Student> _assignedStudents;
+
+    public StudentManagementWindowManageStudentsGroupPage(DataRepository dataRepository, HashSet<Student> assignedStudents)
+    {
+        InitializeComponent();
+        _dataRepository = dataRepository;
+        _assignedStudents = assignedStudents;
+        CourseComboBox.ItemsSource = _dataRepository.Courses;
+        StudentsListView.ItemsSource = _dataRepository.Students;
+    }
+
+    private void AddStudentsToGroup_Click(object sender, RoutedEventArgs e)
+    {
+        Course selectedCourse = CourseComboBox.SelectedItem as Course;
+        string selectedGroupName = GroupComboBox.SelectedItem as string;
+
+        if (selectedCourse != null && !string.IsNullOrEmpty(selectedGroupName))
+        {
+            Group selectedGroup = selectedCourse.Groups.FirstOrDefault(group => group.GroupName == selectedGroupName);
+
+            if (selectedGroup != null)
+            {
+                var selectedStudents = StudentsListView.SelectedItems.OfType<Student>().ToList();
+
+                if (selectedStudents.Count > 0)
+                {
+                    foreach (var student in selectedStudents)
+                    {
+                        if (student.CurrentGroupName == null && !_assignedStudents.Contains(student))
+                        {
+                            student.CurrentGroupName = selectedGroup.GroupName;
+                            selectedGroup.Students.Add(student);
+
+                            _assignedStudents.Add(student);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Student '" + student.StudentFullName + "' is already assigned to a group '" + student.CurrentGroupName + "'", "Error",
+                                MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+
+                    StudentsListView.Items.Refresh();
+
+                    StudentsListView.SelectedItems.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Please, select a student from the list to add to the group", "Error",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("The selected group does not exist for the chosen course.", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+        else
+        {
+            MessageBox.Show("Please, select both a course and a group to add students to", "Error",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    private void DeleteStudentsFromGroup_Click(object sender, RoutedEventArgs e)
+    {
+        var selectedStudents = StudentsListView.SelectedItems.OfType<Student>().ToList();
+
+        if (selectedStudents.Count > 0)
+        {
+            foreach (var student in selectedStudents)
+            {
+                Group selectedGroup = _dataRepository.Groups.FirstOrDefault(group => group.Students.Contains(student));
+
+                if (selectedGroup != null)
+                {
+                    selectedGroup.Students.Remove(student);
+                    student.CurrentGroupName = null;
+
+                    _assignedStudents.Remove(student);
+                }
+                else
+                {
+                    MessageBox.Show("This student is not assigned to any group", "Error",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+            StudentsListView.Items.Refresh();
+
+            StudentsListView.SelectedItems.Clear();
+        }
+        else
+        {
+            MessageBox.Show("Please, select a student from the list to remove it from the group", "Error",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    private void CourseComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        Course selectedCourse = CourseComboBox.SelectedItem as Course;
+
+        if (selectedCourse != null)
+        {
+            GroupComboBox.ItemsSource = selectedCourse.Groups.Select(group => group.GroupName).ToList();
+        }
+        else
+        {
+            GroupComboBox.ItemsSource = null;
+        }
+    }
+}
+
