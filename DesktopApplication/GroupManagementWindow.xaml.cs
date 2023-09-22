74042c227c6115a73ab5e6b2bb558d60c7d6c454
Pecherky Oleg <yogidanfuzo@gmail.com>
2023-09-22 17:23:56 +0300

    Overal heavy modification to "Group Management Window"
    1. Added a new feature: "Create File With Group Info" thath contains a logic for docx/pdf file creation with selected group and course
    2. Refract main window [GroupManagementWindow.xaml] and  [GroupManagementWindow.xaml.cs] to work with new feature
    3. Modified a UI to held new feature implementation
    4. Small changes in other pages UI

--- DesktopApplication/GroupManagementWindow.xaml.cs
+++ DesktopApplication/GroupManagementWindow.xaml.cs
@@ -19,7 +19,8 @@ public partial class GroupManagementWindow : Window
         EditGroupInfoButton.Style = (Style)FindResource("HighlightedButtonStyle");
         CreateGroupButton.Style = (Style)FindResource("NormalButtonStyle");
         DeleteGroupButton.Style = (Style)FindResource("NormalButtonStyle");
-    
+        CreateFileWithGroupInfoButton.Style = (Style)FindResource("NormalButtonStyle");
+
         MainFrame.Content = editGroupInfoPage;
     }
     
@@ -30,6 +31,7 @@ public partial class GroupManagementWindow : Window
         EditGroupInfoButton.Style = (Style)FindResource("NormalButtonStyle");
         CreateGroupButton.Style = (Style)FindResource("HighlightedButtonStyle");
         DeleteGroupButton.Style = (Style)FindResource("NormalButtonStyle");
+        CreateFileWithGroupInfoButton.Style = (Style)FindResource("NormalButtonStyle");
 
         MainFrame.Content = createGroupPage;
     }
@@ -41,7 +43,20 @@ public partial class GroupManagementWindow : Window
         EditGroupInfoButton.Style = (Style)FindResource("NormalButtonStyle");
         CreateGroupButton.Style = (Style)FindResource("NormalButtonStyle");
         DeleteGroupButton.Style = (Style)FindResource("HighlightedButtonStyle");
+        CreateFileWithGroupInfoButton.Style = (Style)FindResource("NormalButtonStyle");
 
         MainFrame.Content = deleteGroupPage;
+    }
+
+    private void CreateFileWithGroup_Click(object sender, RoutedEventArgs e)
+    {
+        var createFileWithGroupPage = new GroupManagementWindowCreateFileWithGroupInfoPage(_dataRepository);
+
+        EditGroupInfoButton.Style = (Style)FindResource("NormalButtonStyle");
+        CreateGroupButton.Style = (Style)FindResource("NormalButtonStyle");
+        DeleteGroupButton.Style = (Style)FindResource("NormalButtonStyle");
+        CreateFileWithGroupInfoButton.Style = (Style)FindResource("HighlightedButtonStyle");
+
+        MainFrame.Content = createFileWithGroupPage;
     }
 }
---
+++ DesktopApplication/GroupManagementWindowCreateFileWithGroupInfoPage.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System.Windows;
+using Microsoft.Win32;
+using System.Windows.Controls;
+using System.Linq;
+using System.Text;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
+using PdfSharp.Pdf;
+using PdfSharp.Drawing;
+
+
+
+namespace DesktopApplication;
+
+
+public partial class GroupManagementWindowCreateFileWithGroupInfoPage : Page
+{
+    private DataRepository _dataRepository;
+
+    public GroupManagementWindowCreateFileWithGroupInfoPage(DataRepository dataRepository)
+    {
+        InitializeComponent();
+        _dataRepository = dataRepository;
+        CourseComboBox.ItemsSource = _dataRepository.Courses;
+    }
+
+    private void CreateGroupInfoDocxFile_Click(object sender, RoutedEventArgs e)
+    {
+        Course selectedCourse = CourseComboBox.SelectedItem as Course;
+        string selectedGroupName = GroupComboBox.SelectedItem as string;
+
+        if (selectedCourse != null && !string.IsNullOrEmpty(selectedGroupName))
+        {
+            Group selectedGroup = selectedCourse.Groups.FirstOrDefault(group => group.GroupName == selectedGroupName);
+
+            if (selectedGroup != null)
+            {
+                if (!selectedGroup.Students.Any())
+                {
+                    MessageBox.Show("The selected group is empty. There are no students to include in the document", "Empty Group",
+                        MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+
+                var saveNewFileDialog = new SaveFileDialog
+                {
+                    Filter = "Word Documents (.docx)|*.docx",
+                    DefaultExt = ".docx",
+                    FileName = $"{selectedCourse.CourseName} {selectedGroupName} Students List"
+                };
+
+                if (saveNewFileDialog.ShowDialog() == true)
+                {
+                    string filePath = saveNewFileDialog.FileName;
+
+                    using var doc = DocX.Create(filePath);
+
+                    doc.InsertParagraph(selectedCourse.CourseName).Bold().FontSize(18).Alignment = Alignment.center;
+                    doc.InsertParagraph(selectedGroup.GroupName).Bold().FontSize(14).Alignment = Alignment.center;
+
+                    var students = selectedGroup.Students.Select(student => student.StudentFullName).ToList();
+
+                    for (int i = 0; i < students.Count; i++)
+                    {
+                        doc.InsertParagraph($"{i + 1}. {students[i]}");
+                    }
+
+                    doc.Save();
+
+                    MessageBox.Show("(.docx) file with selected group successfully generated!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("The selected group does not exist for the chosen course", "Error", 
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        else
+        {
+            MessageBox.Show("Please, select both a course and a group to save students list from", "Error",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    private void CreateGroupInfoPdfFile_Click(object sender, RoutedEventArgs e)
+    {
+        Course selectedCourse = CourseComboBox.SelectedItem as Course;
+        string selectedGroupName = GroupComboBox.SelectedItem as string;
+
+        if (selectedCourse != null && !string.IsNullOrEmpty(selectedGroupName))
+        {
+            Group selectedGroup = selectedCourse.Groups.FirstOrDefault(group => group.GroupName == selectedGroupName);
+
+            if (selectedGroup != null)
+            {
+                if (!selectedGroup.Students.Any())
+                {
+                    MessageBox.Show("The selected group is empty. There are no students to include in the document", "Empty Group",
+                        MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+
+                var saveNewFileDialog = new SaveFileDialog
+                {
+                    Filter = "PDF Files (.pdf)|*.pdf",
+                    DefaultExt = ".pdf",
+                    FileName = $"{selectedCourse.CourseName} {selectedGroupName} Students List"
+                };
+
+                if (saveNewFileDialog.ShowDialog() == true)
+                {
+                    string filePath = saveNewFileDialog.FileName;
+
+                    var document = new PdfDocument();
+                    var page = document.AddPage();
+                    var gfx = XGraphics.FromPdfPage(page);
+
+                    var courseTitleFont = new XFont("Arial", 18, XFontStyle.Bold);
+                    var groupTitleFont = new XFont("Arial", 14, XFontStyle.Bold);
+                    var textFont = new XFont("Arial", 12, XFontStyle.Regular);
+
+                    gfx.DrawString(selectedCourse.CourseName, courseTitleFont, XBrushes.Black, new XRect(0, 40, page.Width, 0), XStringFormats.TopCenter);
+                    gfx.DrawString(selectedGroup.GroupName, groupTitleFont, XBrushes.Black, new XRect(0, 70, page.Width, 0), XStringFormats.TopCenter);
+
+                    var students = selectedGroup.Students.Select(student => student.StudentFullName).ToList();
+
+                    for (int i = 0; i < students.Count; i++)
+                    {
+                        byte[] utf8BytesConvert = Encoding.UTF8.GetBytes($"{i + 1}. {students[i]}");
+                        string utf8Text = Encoding.UTF8.GetString(utf8BytesConvert);
+
+                        gfx.DrawString(utf8Text, textFont, XBrushes.Black, new XRect(50, 100 + i * 20, page.Width - 100, 0));
+                    }
+
+                    document.Save(filePath);
+
+                    MessageBox.Show("(.pdf) file with selected group successfully generated!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("The selected group does not exist for the chosen course", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        else
+        {
+            MessageBox.Show("Please, select both a course and a group to save students list from", "Error",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    private void CourseComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        Course selectedCourse = CourseComboBox.SelectedItem as Course;
+
+        if (selectedCourse != null)
+        {
+            GroupComboBox.ItemsSource = selectedCourse.Groups.Select(group => group.GroupName).ToList();
+        }
+        else
+        {
+            GroupComboBox.ItemsSource = null;
+        }
+    }
+}
