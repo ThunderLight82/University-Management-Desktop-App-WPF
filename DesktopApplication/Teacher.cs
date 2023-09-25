f48ab2581c3056f9e29669b17d0460d5a9470895
Pecherky Oleg <yogidanfuzo@gmail.com>
2023-09-25 16:23:51 +0300

    Added new string in [Teacher.cs] for future "Teacher Management  Window" method implementation

--- DesktopApplication/Teacher.cs
+++ DesktopApplication/Teacher.cs
@@ -3,6 +3,10 @@ namespace DesktopApplication;
 public class Teacher
 { 
     public int TeacherId { get; set; } 
+
     public string TeacherFullName { get; set; }
+
     public bool IsCorrespondence { get; set; }
+
+    public string CurrentGroupCurationName { get; set; }
 }
