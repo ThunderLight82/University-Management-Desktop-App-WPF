47a750afeb3fa388770afd97d1f46f1b8b11a953
Pecherky Oleg <yogidanfuzo@gmail.com>
2023-09-25 16:39:35 +0300

    Creation of in "Edit Teacher" and "Change Teacher Data" in "Teachers Manegement" entry
    1. Added a new class and .xaml [TeacherManagementWindowChangeTeacherDataPage] and [TeacherManagementWindowEditTeacherPage] for core logic behind "Teacher Management Window" thath held a func like: changing existing teacher data info and creation or deletion teachers from datarepo
    2. Added a UI with user-friendly teacher listview like in "Student Management" acording to new buttons

---
+++ DesktopApplication/TeacherManagementWindowChangeTeacherDataPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DesktopApplication;
+
+public partial class TeacherManagementWindowChangeTeacherDataPage : Page
+{
+    private DataRepository _dataRepository;
+    public TeacherManagementWindowChangeTeacherDataPage(DataRepository dataRepository)
+    {
+        InitializeComponent();
+        _dataRepository = dataRepository;
+        TeachersListView.ItemsSource = _dataRepository.Teachers;
+    }
+
+    private void ChangeTeacherNameAndWorkInfo_Click(object sender, RoutedEventArgs e)
+    {
+        if (TeachersListView.SelectedItem is Teacher selectedTeacher)
+        {
+            string changedTeacherFullname = ChangeTeacherFullNameTextBox.Text.Trim();
+
+            if (!string.IsNullOrWhiteSpace(changedTeacherFullname))
+            {
+                selectedTeacher.TeacherFullName = changedTeacherFullname;
+
+                ComboBoxItem selectedComboBoxItem = IsCorrespondence.SelectedItem as ComboBoxItem;
+
+                if (selectedComboBoxItem != null)
+                {
+                    string selectedItemContent = selectedComboBoxItem.Content.ToString();
+                    selectedTeacher.IsCorrespondence = selectedItemContent == "Yes";
+                }
+
+                TeachersListView.Items.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("Please, enter a valid teacher name", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        else
+        {
+            MessageBox.Show("Please, select teacher from list first to update info", "Error",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    private void FillInfoBlockWithSelectedTeacherFromListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (TeachersListView.SelectedItem is Teacher selectedTeacher)
+        {
+            ChangeTeacherFullNameTextBox.Text = selectedTeacher.TeacherFullName;
+
+            IsCorrespondence.SelectedIndex = selectedTeacher.IsCorrespondence ? 0 : 1;
+        }
+    }
+}
---
+++ DesktopApplication/TeacherManagementWindowEditTeacherPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DesktopApplication;
+
+public partial class TeacherManagementWindowEditTeacherPage : Page
+{
+    private DataRepository _dataRepository;
+    public TeacherManagementWindowEditTeacherPage(DataRepository dataRepository)
+    {
+        InitializeComponent();
+        _dataRepository = dataRepository;
+        TeachersListView.ItemsSource = _dataRepository.Teachers;
+    }
+
+    private void AddTeacher_Click(object sender, RoutedEventArgs e)
+    {
+        string newTeacherFullName = NewTeacherFullNameTextBox.Text.Trim();
+
+        if (!string.IsNullOrWhiteSpace(newTeacherFullName))
+        {
+            bool newTeacherNameAlreadyExists = _dataRepository.Teachers.Any(teacher =>
+                teacher.TeacherFullName.Equals(newTeacherFullName, StringComparison.OrdinalIgnoreCase));
+
+            if (newTeacherNameAlreadyExists)
+            {
+                var duplicateTeacherQuestion = MessageBox.Show(
+                    "A teacher with the same name already exists. Do you want to add this teacher anyway?",
+                    "Duplication name",
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Question);
+
+                if (duplicateTeacherQuestion == MessageBoxResult.No)
+                {
+                    return;
+                }
+            }
+
+            int lastNewestTeacherId = _dataRepository.Teachers.Max(teacher => teacher.TeacherId);
+
+            var newTeacher = new Teacher
+            {
+                TeacherId = lastNewestTeacherId + 1,
+                TeacherFullName = newTeacherFullName,
+                IsCorrespondence = false
+            };
+
+            _dataRepository.Teachers.Add(newTeacher);
+
+            NewTeacherFullNameTextBox.Clear();
+
+            TeachersListView.Items.Refresh();
+        }
+        else
+        {
+            MessageBox.Show("Please, enter a valid teacher name", "Error",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+
+    private void DeleteTeacher_Click(object sender, RoutedEventArgs e)
+    {
+        Teacher selectedTeacher = TeachersListView.SelectedItem as Teacher;
+
+        if (selectedTeacher != null)
+        {
+            Group associatedGroup = _dataRepository.Groups.FirstOrDefault(group => group.GroupCurator.Contains(selectedTeacher));
+
+            if (associatedGroup != null)
+            {
+                associatedGroup.GroupCurator.Remove(selectedTeacher);
+            }
+
+            selectedTeacher.CurrentGroupCurationName = null;
+
+            _dataRepository.Teachers.Remove(selectedTeacher);
+
+            TeachersListView.Items.Refresh();
+        }
+        else
+        {
+            MessageBox.Show("Please, select a teacher from the list below to remove", "Error",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
