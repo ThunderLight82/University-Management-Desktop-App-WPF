da11c0284a16bc5c94c52f1dbbbebdfaca89421b
Pecherky Oleg <yogidanfuzo@gmail.com>
2023-09-25 16:28:31 +0300

    Initial creation of "Teacher Management Window" and UI for it
    1. Writing core logic for next two pages: "Change Teachers Data" and "Esit Teachers"
    2. Created a button for switching betwen pages. (Overall UI creation)

---
+++ DesktopApplication/TeacherManagementWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace DesktopApplication;
+
+public partial class TeacherManagementWindow : Window
+{
+    private DataRepository _dataRepository;
+
+    public TeacherManagementWindow(DataRepository dataRepository)
+    {
+        InitializeComponent();
+        _dataRepository = dataRepository;
+    }
+
+    private void EditTeacher_Click(object sender, RoutedEventArgs e)
+    {
+        var editTeacherPage = new TeacherManagementWindowEditTeacherPage(_dataRepository);
+
+        EditTeacherButton.Style = (Style)FindResource("HighlightedButtonStyle");
+        ChangeTeacherDataButton.Style = (Style)FindResource("NormalButtonStyle");
+
+        MainFrame.Content = editTeacherPage;
+    }
+
+    private void ChangeTeacherData_Click(object sender, RoutedEventArgs e)
+    {
+        var changeTeacherDataPage = new TeacherManagementWindowChangeTeacherDataPage(_dataRepository);
+
+        EditTeacherButton.Style = (Style)FindResource("NormalButtonStyle");
+        ChangeTeacherDataButton.Style = (Style)FindResource("HighlightedButtonStyle");
+
+        MainFrame.Content = changeTeacherDataPage;
+    }
+}
