a57abdc4d3d89ecd832bd104307cbab704d238d2
Pecherky Oleg <yogidanfuzo@gmail.com>
2023-09-25 16:26:12 +0300

    Added a "GroupCurationDisplay" for future implementation in "Teacher Management Window"

--- DesktopApplication/Group.cs
+++ DesktopApplication/Group.cs
@@ -1,17 +1,34 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace DesktopApplication;
 
 public class Group
 {
     public int GroupId { get; set; }
+
     public string GroupName { get; set; }
+
     public List<Student> Students { get; set; }
-    public string GroupCuratorName { get; set; }
-    public Teacher GroupCurator { get; set; }
-    
+
+    public List<Teacher> GroupCurator { get; set; }
+
+    public string GroupCuratorDisplay
+    {
+        get
+        {
+            if (GroupCurator == null || GroupCurator.Count == 0)
+            {
+                return "";
+            }
+
+            return string.Join(", ", GroupCurator.Select(curator => curator.TeacherFullName));
+        }
+    }
+
     public Group()
     {
         Students = new List<Student>();
+        GroupCurator = new List<Teacher>();
     }
 }
